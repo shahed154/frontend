03658082c14c5d86b8353742ccdecd34f50ef22d
shahed154 <shahedadnan@hotmail.com>
2025-05-19 17:57:33 -0400

    link update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://gamebrowserapp-1.onrender.com'
+const BASE_URL = 'https://gamebrowserapp.onrender.com'
 
 const api = axios.create({
   baseURL: BASE_URL,
